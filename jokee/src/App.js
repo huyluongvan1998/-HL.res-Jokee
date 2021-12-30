31a272eb88e2d278e49edb322322f2e73e3450c2
Huy Luong <huylvgcs190132@fpt.edu.vn>
2021-12-30 09:09:43 +0700

    [Jokee] Update Scss, Build UI and styling

--- jokee/src/App.js
+++ jokee/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+import brand from './images/joke-brand.svg';
+import user from './images/user.svg';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div classNameName='App'>
+      <header>
+        <div className='container'>
+          <div className='brand'>
+            <img src={brand} alt='err' />
+          </div>
+          <div className='user'>
+            <div className='user-profile '>
+              <div className='user-profile-title grey-1'>Hancrafted By</div>
+              <div className='user-profile-name'>Jack</div>
+            </div>
+            <div className='user-image'>
+              <img src={user} alt='err' />
+            </div>
+          </div>
+        </div>
       </header>
+      <main>
+        <div className='banner green-bg-1'>
+          <div className='banner-content'>
+            <h1 className='title white-700'>A joke a day keep doctor away</h1>
+            <p className='quote white-400'>
+              If you joke wrong way, your teeth have to pay. (Serious)
+            </p>
+          </div>
+        </div>
+        <div className='joke'>
+          <div className='joke-content'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic ipsa
+            reiciendis laborum facere quas deleniti at quisquam amet nobis,
+            aspernatur culpa velit alias soluta. Deserunt suscipit dolore velit
+            fuga. Reprehenderit quibusdam earum facilis ducimus sequi, labore
+            accusamus velit obcaecati laboriosam accusantium nobis et aut,
+            excepturi, beatae nostrum quidem. In illo neque dolores odio
+            accusamus ratione deleniti, voluptatibus facilis id dignissimos,
+            doloremque adipisci cupiditate esse cum debitis, commodi consequatur
+            nesciunt eos molestiae. Exercitationem ullam nemo laudantium minus,
+            assumenda fuga. Laborum dolores ratione non cumque mollitia corrupti
+            sit necessitatibus sequi a ducimus voluptatibus eos, minus error ab,
+            aspernatur officia maiores veniam quaerat?
+          </div>
+          <div className='joke-button'>
+            <div className='button btn-primary joke-button-funny'>
+              this is Funny!
+            </div>
+            <div className='button btn-success joke-button-bad'>
+              this is not Funny.
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer></footer>
     </div>
   );
-}
+};
 
 export default App;
